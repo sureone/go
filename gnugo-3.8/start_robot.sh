21483bc758edb3a757b7b393109d3ad905a242fd
Yang Xiaowang <yang.xiaowang@wcare.cn>
2017-02-21 09:27:38 +0800

    robot startup

---
+++ gnugo-3.8/start_robot.sh
@@ -0,0 +1,6 @@
+nohup ./interface/gnugo --mplayer robot-1 --desk 1 --serverip 127.0.0.1 --serverport 8090 --level 10 &
+nohup ./interface/gnugo --mplayer robot-2 --desk 2 --serverip 127.0.0.1 --serverport 8090 --level 10 &
+nohup ./interface/gnugo --mplayer robot-3 --desk 3 --serverip 127.0.0.1 --serverport 8090 --level 10 &
+nohup ./interface/gnugo --mplayer robot-4 --desk 4 --serverip 127.0.0.1 --serverport 8090 --level 1 &
+nohup ./interface/gnugo --mplayer robot-5 --desk 5 --serverip 127.0.0.1 --serverport 8090 --level 1 &
+nohup ./interface/gnugo --mplayer robot-6 --desk 6 --serverip 127.0.0.1 --serverport 8090 --level 1 &
