ef44f84d1ab44e2008f05c3a315238b9ddc97eaa
sureone <yang.xiaowang@wcare.cn>
2017-03-23 08:07:10 +0800

    database change

--- ama/ama.sql
+++ ama/ama.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.1.12
+-- version 4.2.11
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: 2017-03-17 08:33:24
--- 服务器版本： 5.6.16
--- PHP Version: 5.5.11
+-- Generation Time: 2017-03-23 01:05:27
+-- 服务器版本： 5.6.21
+-- PHP Version: 5.6.3
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -39,14 +39,16 @@ CREATE TABLE IF NOT EXISTS `datas` (
 --
 
 CREATE TABLE IF NOT EXISTS `things` (
-  `ID` bigint(20) NOT NULL AUTO_INCREMENT,
+`ID` bigint(20) NOT NULL,
   `ups` int(11) NOT NULL DEFAULT '0',
   `downs` int(11) NOT NULL DEFAULT '0',
   `type` varchar(12) NOT NULL,
   `cdate` int(11) NOT NULL,
   `udate` int(11) NOT NULL,
-  PRIMARY KEY (`ID`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+  `title` varchar(1024) CHARACTER SET utf8 NOT NULL,
+  `author` varchar(100) NOT NULL,
+  `content` text CHARACTER SET utf8
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
@@ -55,16 +57,14 @@ CREATE TABLE IF NOT EXISTS `things` (
 --
 
 CREATE TABLE IF NOT EXISTS `users` (
-  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+`id` bigint(20) NOT NULL,
   `userid` varchar(64) NOT NULL,
   `name` varchar(64) NOT NULL,
   `passwd` varchar(128) NOT NULL,
   `email` varchar(128) DEFAULT NULL,
   `mobile` varchar(18) DEFAULT NULL,
-  `status` varchar(24) NOT NULL DEFAULT 'normal',
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `userid` (`userid`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COMMENT='用户表' AUTO_INCREMENT=2 ;
+  `status` varchar(24) NOT NULL DEFAULT 'normal'
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8 COMMENT='用户表';
 
 --
 -- 转存表中的数据 `users`
@@ -73,6 +73,36 @@ CREATE TABLE IF NOT EXISTS `users` (
 INSERT INTO `users` (`id`, `userid`, `name`, `passwd`, `email`, `mobile`, `status`) VALUES
 (1, 'sureone', '小网', '81dc9bdb52d04dc20036dbd8313ed055', 'yang.xiaowang@wcare.cn', NULL, 'normal');
 
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `things`
+--
+ALTER TABLE `things`
+ ADD PRIMARY KEY (`ID`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+ ADD PRIMARY KEY (`id`), ADD UNIQUE KEY `userid` (`userid`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `things`
+--
+ALTER TABLE `things`
+MODIFY `ID` bigint(20) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=2;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
