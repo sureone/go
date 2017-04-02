b83ea9ba47f31de24c18ab62bc2daabeec75cf5c
sureone <yang.xiaowang@wcare.cn>
2017-04-02 15:12:14 +0800

    ui dev

---
+++ ama/static/js/message-inbox.js
@@ -0,0 +1,17 @@
+function handleFormSubmit(form){
+
+    $$.ajaxPostForm(
+        //action
+        $(form).attr('action'),
+        //form id
+        '#'+$(form).attr('id'),
+        //addition params
+        null,
+        //callback
+        function(sucess,data){
+            window.location.href=window.location.href;
+        }
+    )
+
+    
+}
