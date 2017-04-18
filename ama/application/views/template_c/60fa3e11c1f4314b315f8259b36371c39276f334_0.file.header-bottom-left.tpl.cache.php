ed8a454543811c5d79fb178f138f6f341de85722
Yang Xiaowang <yang.xiaowang@wcare.cn>
2017-04-18 10:08:58 +0800

    logo

--- ama/application/views/template_c/60fa3e11c1f4314b315f8259b36371c39276f334_0.file.header-bottom-left.tpl.cache.php
+++ ama/application/views/template_c/60fa3e11c1f4314b315f8259b36371c39276f334_0.file.header-bottom-left.tpl.cache.php
@@ -1,30 +1,33 @@
 <?php
-/* Smarty version 3.1.30, created on 2017-04-18 03:46:49
+/* Smarty version 3.1.30, created on 2017-04-18 04:08:29
   from "D:\work\go\ama\application\views\header-bottom-left.tpl" */
 
 /* @var Smarty_Internal_Template $_smarty_tpl */
 if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
   'version' => '3.1.30',
-  'unifunc' => 'content_58f57009cb8695_07830192',
+  'unifunc' => 'content_58f5751d376921_04250071',
   'has_nocache_code' => false,
   'file_dependency' => 
   array (
     '60fa3e11c1f4314b315f8259b36371c39276f334' => 
     array (
       0 => 'D:\\work\\go\\ama\\application\\views\\header-bottom-left.tpl',
-      1 => 1492480006,
+      1 => 1492480921,
       2 => 'file',
     ),
   ),
   'includes' => 
   array (
+    'file:common/page-logo.tpl' => 1,
   ),
 ),false)) {
-function content_58f57009cb8695_07830192 (Smarty_Internal_Template $_smarty_tpl) {
-$_smarty_tpl->compiled->nocache_hash = '2203258f57009c79e80_55275357';
+function content_58f5751d376921_04250071 (Smarty_Internal_Template $_smarty_tpl) {
+$_smarty_tpl->compiled->nocache_hash = '1357758f5751d338117_00158918';
 ?>
 <div id="header-bottom-left">
-    <span class="hover pagename"><a href="./"><img src="./static/images/logo.gif" alt="后园小亭" height="20"></a></span>
+    <?php $_smarty_tpl->_subTemplateRender("file:common/page-logo.tpl", $_smarty_tpl->cache_id, $_smarty_tpl->compile_id, 9999, $_smarty_tpl->cache_lifetime, array(), 0, false);
+?>
+
     <ul class="tabmenu ">
         <li <?php if ($_smarty_tpl->tpl_vars['page']->value == "hot") {?>class="selected"<?php }?>><a href="./v/hot" class="choice">热门</a></li>
         <li <?php if ($_smarty_tpl->tpl_vars['page']->value == "new") {?>class="selected"<?php }?>><a href="./v/news" class="choice">最新</a></li>
