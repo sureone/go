aa11faa8ec5d30c795f82c60451f9c0c43c28fb3
yangxiaowang <sureone@yeah.net>
2017-05-04 22:17:36 +0800

    wx support

--- ama/application/controllers/Api.php
+++ ama/application/controllers/Api.php
@@ -18,6 +18,10 @@ class Api extends CI_Controller {
      * map to /index.php/welcome/<method_name>
      * @see https://codeigniter.com/user_guide/general/urls.html
      */
+    protected $appid="wx9ec950ea9d8e2f64";
+    protected $secret="b75776bd031b6e93177efe64ec281e86";
+    protected $sessionkey;
+    
     function __construct()
     {       
         // Call the Model constructor       
@@ -29,10 +33,48 @@ class Api extends CI_Controller {
         $this->load->database();
         $this->load->helper('file');
         $this->load->library('ci_smarty');
+        $this->load->library('wx_crypt');
     }
-    public function wxlogin($code){
-       
-        echo $code;
+    function curl_request($url,$post='',$cookie='', $returnCookie=0){
+        $curl = curl_init();
+        curl_setopt($curl, CURLOPT_URL, $url);
+        curl_setopt($curl, CURLOPT_USERAGENT, 'Mozilla/5.0 (compatible; MSIE 10.0; Windows NT 6.1; Trident/6.0)');
+        curl_setopt($curl, CURLOPT_FOLLOWLOCATION, 1);
+        curl_setopt($curl, CURLOPT_AUTOREFERER, 1);
+        curl_setopt($curl, CURLOPT_REFERER, "http://XXX");
+        if($post) {
+            curl_setopt($curl, CURLOPT_POST, 1);
+            curl_setopt($curl, CURLOPT_POSTFIELDS, http_build_query($post));
+        }
+        if($cookie) {
+            curl_setopt($curl, CURLOPT_COOKIE, $cookie);
+        }
+        curl_setopt($curl, CURLOPT_HEADER, $returnCookie);
+        curl_setopt($curl, CURLOPT_TIMEOUT, 10);
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+        $data = curl_exec($curl);
+        if (curl_errno($curl)) {
+            return curl_error($curl);
+        }
+        curl_close($curl);
+        if($returnCookie){
+            list($header, $body) = explode("\r\n\r\n", $data, 2);
+            preg_match_all("/Set\-Cookie:([^;]*);/", $header, $matches);
+            $info['cookie']  = substr($matches[1][0], 1);
+            $info['content'] = $body;
+            return $info;
+        }else{
+            return $data;
+        }
+    }
+   
+    public function wxlogin($code=""){
+	$url = "https://api.weixin.qq.com/sns/jscode2session?appid={$this->appid}&secret={$this->secret}&js_code={$code}&grant_type=authorization_code";
+      	$data = file_get_contents($url);	 
+        $jobj = json_decode($data);
+        $errCode = $this->wx_crypt->decryptData($this->appid,$jobj->{'session_key'},$_GET['encryptedData'], $_GET['iv'], $data );
+	echo $errCode;
+
     }
 
     public function index()
